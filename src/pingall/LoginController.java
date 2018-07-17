1c1b2047f057e573178ce0535917b49230e97c29
Aman Kumar Nirala <35570939+amannirala13@users.noreply.github.com>
2018-07-17 15:41:39 +0530

    Tried Append write mode to solve the history log issue. Result : Failed

---
+++ src/pingall/LoginController.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pingall;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
+
+/**
+ *
+ * @author hp
+ */
+public class LoginController extends Pingall implements Initializable {
+    
+      @FXML
+    private JFXTextField userid;
+
+    @FXML
+    private JFXPasswordField password;
+
+    @FXML
+    private JFXButton loginBtn;
+
+    @FXML
+    private Hyperlink signupBtn;
+
+    
+    
+    @FXML
+    private void loginBtnPressed(ActionEvent event) throws IOException, Exception {
+        if(checkLogin())
+        {
+            setDashboard();
+        
+        }
+        
+        else
+        {
+                System.out.println("Not found");
+                }
+    
+    }
+    
+    private boolean checkLogin() throws SQLException, Exception
+    {
+        boolean auth = false;
+          try{ Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/accounts","root","Amannirala_1361"); 
+            try (Statement statement = connect.createStatement(); ResultSet result = statement.executeQuery("select * from users")) {
+                while(result.next())
+                {
+                   if((result.getString("userID")).equalsIgnoreCase(userid.getText()))
+                    {
+                        auth = result.getString("password").equalsIgnoreCase(password.getText());
+                    }
+                }
+            }
+}
+          catch(SQLException e)
+          {
+           System.out.println(e);
+          }
+        return auth;
+   
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/pingall/Pingall.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pingall;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ * CopyRightsReserved@2018_ASDEV
+ * @author Aman Nirala
+ */
+public class Pingall extends Application {
+   static Stage MainActivity;      //MainActivity: Set all scenes to this. >>>> eg: Stage stage = MainActivity; stage.setScene(scene);
+ 
+    @Override
+    public void start(Stage LoginActivity) throws Exception {
+        MainActivity = LoginActivity;
+        Parent login = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(login);
+        MainActivity.initStyle(StageStyle.TRANSPARENT);
+        MainActivity.setScene(scene);
+        MainActivity.show();
+}
+    
+    //Serts dashboard on the Stage
+    public void setDashboard() throws Exception
+    {
+        Stage DashboardActivity = MainActivity;
+        Parent Dashboard = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+        Scene scene = new Scene(Dashboard);
+        DashboardActivity.setScene(scene);
+        DashboardActivity.show();
+    }
+    
+    
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
